efacc442010dc2a201e6d277ddecfce1917a38f5
mylinux5879 <idcnishimiya5378@gmail.com>
2022-04-09 07:12:48 +0900

    sshでーた表示作成中

--- index.js
+++ index.js
@@ -2,21 +2,12 @@ const express = require('express');
 const v8 = require('v8');
 const si = require('systeminformation');
 const ping = require('ping');
-
 const config = require("./params.js");
-const { setgid } = require('process');
 
 const app = express();
+app.set("view engine", "ejs")
 app.use(express.static('static'));
-
-app.get("/" , (req, res) => {
-    res.render(__dirname + "/index.ejs", config)
-})
-
-app.post("/empty", (req, res) => {
-    res.end();
-})
-
+const logdb = require("./database")
 
 // service ping check
 let services = [];
@@ -35,9 +26,9 @@ let stat = {};
 setInterval(async() =>  {
     stat = {
         cpu : {
-            speed : (await si.cpuCurrentSpeed()).cores,
-            temp  : (await si.cpuTemperature()).cores,
-            load  : await si.currentLoad(),
+          speed : (await si.cpuCurrentSpeed()).cores,
+          temp  : (await si.cpuTemperature()).cores,
+          load  : await si.currentLoad(),
         },
         memory : await si.mem(),
         ctime  : Date.now(),
@@ -51,10 +42,57 @@ setInterval(async() =>  {
     }
 }, config.performance_check_cron);
 
-app.post("/p", async(req, res) => {
-    res.json(stat);
+app.get("/" , (req, res) => { res.render("service", config) })
+app.post("/empty", (req, res) => {res.end();})
+app.post("/p", async(req, res) => { res.json(stat);})
+
+app.get("/auth", async(req, res)=> {
+  const n = 100;
+  const page = 1;
+  const result = await logdb.get_data(n, page); 
+  console.log(result);
+  res.render("auth_log", {type:"list", result:result });
 })
-  
+
+app.get("/ssh", async(req, res)=> {
+  res.render("ssh_analisys", {});
+})
+
+app.get("/ssh-table", async(req, res)=> {
+  const n = 100;
+  const page = 1;
+  const result = await logdb.get_ssh_auth_data(n, page); 
+  console.log(result);
+  res.render("auth_log", {type:"list", result:result });
+})
+
+app.get("/ssh_analisis", async(req, res) => {
+  const data = await logdb.get_ssh_analitics();
+  res.json(data);
+})
+
+app.get("/refresh_data", async(req, res)=> {
+  res.send("ok")
+  await logdb.create_table();
+  // await logdb.clear_data();
+  const pages = [
+    // {name:"/var/log/auth.log", zipped:false },
+    // {name:"/var/log/auth.log.2", zipped:false },
+    // {name:"/var/log/auth.log.2.gz", zipped:true },
+    // {name:"/var/log/auth.log.3.gz", zipped:true },
+    {name:"./tmp/auth.log", zipped:false },
+    {name:"./tmp/auth.log.1", zipped:false },
+    {name:"./tmp/auth.log.2.gz", zipped:true },
+    {name:"./tmp/auth.log.3.gz", zipped:true },
+  ]
+  for await (f of pages){
+    await logdb.insert_syslog_data(f.name, f.zipped);
+    console.log(f.name);
+  }
+  console.log("hoge")
+  await logdb.insert_nginx_data("./tmp/access.log", false);
+})
+
 app.use((req, res, next) => {
     res.status(404).send("<h1>404 Page Not Found</h1>");
 });
